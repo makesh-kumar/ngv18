8a9ec18fa2f991ab89c6a5d3c1fac061bdb3c0f7
Makesh <mynameismakesh@gmail.com>
2024-05-23 21:21:34 +0530

    initial files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,JsonPipe,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ngv18';
+
+  cdRef = inject(ChangeDetectorRef);
+  fb = inject(FormBuilder);
+  
+  userForm!: FormGroup;
+  count = signal(0);
+
+  
+  ngOnInit(): void  {
+    setInterval(() => {
+      this.count.update(count => count + 1);
+    // this.cdRef.markForCheck();
+    },1000);
+
+    this.initForm()
+  }
+
+
+  initForm(): void {
+    this.userForm = this.fb.group({
+      name: [''],
+      age: [2]
+    })
+
+    this.userForm.events.subscribe(console.log)
+  }
+
+  submitForm(): void {
+    console.log(this.userForm)
+  }
 }
